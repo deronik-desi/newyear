1649ee11bf67eaaff6cd3ad3e6f72d134f2382d2
Lev <Lev_solomatin@ukr.net>
2025-12-28 20:46:39 +0200

    initial commit

---
+++ cristmas.js
@@ -0,0 +1,44 @@
+const img = document.getElementById("img")
+document.addEventListener("DOMContentLoaded" , () => {
+img.style.left = "0";
+});
+const snow = document.getElementById("snow");
+document.addEventListener("DOMContentLoaded",()=>{
+snow.style.right = "20%";
+snow.style.top = "150%"
+});
+const snow2 = document.getElementById("snow2");
+document.addEventListener("DOMContentLoaded",()=>{
+snow2.style.right = "30%";
+snow2.style.top = "150%"
+});
+const snow3 = document.getElementById("snow3");
+document.addEventListener("DOMContentLoaded",()=>{
+snow3.style.right = "40%";
+snow3.style.top = "150%"
+});
+const snow4 = document.getElementById("snow4");
+document.addEventListener("DOMContentLoaded",()=>{
+snow4.style.right = "50%";
+snow4.style.top = "150%"
+});
+const snow5 = document.getElementById("snow5");
+document.addEventListener("DOMContentLoaded",()=>{
+snow5.style.right = "60%";
+snow5.style.top = "150%"
+});
+const snow6 = document.getElementById("snow6");
+document.addEventListener("DOMContentLoaded",()=>{
+snow6.style.right = "70%";
+snow6.style.top = "150%"
+});
+const snow7 = document.getElementById("snow7");
+document.addEventListener("DOMContentLoaded",()=>{
+snow7.style.right = "80%";
+snow7.style.top = "150%"
+});
+const snow8 = document.getElementById("snow8");
+document.addEventListener("DOMContentLoaded",()=>{
+snow8.style.right = "90%";
+snow8.style.top = "150%"
+});
